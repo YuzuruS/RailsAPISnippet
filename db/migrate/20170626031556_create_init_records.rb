b805fee8a909a758a415e436a0659ca7923c4b3c
sfukai30 <sfukai1218@gmail.com>
2017-06-26 12:23:15 +0900

    modified to use mysql

---
+++ db/migrate/20170626031556_create_init_records.rb
@@ -0,0 +1,20 @@
+class CreateInitRecords < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.timestamps
+    end
+    create_table :products do |t|
+      t.string :name
+      t.integer :price, default: 0
+      t.timestamps
+    end
+    create_table :logs do |t|
+      t.references :user
+      t.references :product
+      t.date :target_date
+      t.timestamps
+    end
+    add_index :logs, [:user_id, :product_id], unique: true
+  end
+end
